9370fbf70717c9032ad3c50048a2bd81023146b2
ravellys <lucas.ravellys@ufpe.br>
2020-09-10 00:00:38 -0300

    criados arquivos para deploy do heroku

--- pypro/settings.py
+++ pypro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@aleq#(z9qz703xk7&ddivfu4jgo&-=ahy$gpozy(n0w4b-w3#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
