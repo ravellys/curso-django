b5688911d999cfd48b870fd86acf30b488ff201d
ravellys <lucas.ravellys@ufpe.br>
2020-09-12 02:18:01 -0300

    collectfast3

--- pypro/settings.py
+++ pypro/settings.py
@@ -146,6 +146,7 @@ if AWS_ACCESS_KEY_ID:
     AWS_QUERYSTRING_AUTH = True
 
     COLLECTFAST_ENABLED = True
+    COLLECTFAST_STRATEGY = 'collectfast.strategies.boto3.Boto3Strategy'
 
     AWS_S3_CUSTOM_DOMAIN = None
     # AWS_DEFAULT_ACL = 'private'
