a3828fd4378feb5b21480f8b97ddce0633343b6d
ravellys <lucas.ravellys@ufpe.br>
2020-09-12 02:06:31 -0300

    collectfast2

--- pypro/settings.py
+++ pypro/settings.py
@@ -148,7 +148,7 @@ if AWS_ACCESS_KEY_ID:
     COLLECTFAST_ENABLED = True
 
     AWS_S3_CUSTOM_DOMAIN = None
-    AWS_DEFAULT_ACL = 'private'
+    # AWS_DEFAULT_ACL = 'private'
 
     # Static Assets
     # ------------------------------------------------------------------------------
