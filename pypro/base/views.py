9a344d86826bd103a9507b2bceba9fb4553826a0
ravellys <lucas.ravellys@ufpe.br>
2020-09-15 00:04:19 -0300

    instalando arquivos estaticos

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -1,7 +1,6 @@
-from django.http import HttpResponse
-# from django.shortcuts import render
+# from django.http import HttpResponse
+from django.shortcuts import render
 
 
-# Create your views here.
-def home(requests):
-    return HttpResponse('<html><body>olá django</body></html>', content_type='text/html')
+def home(request):
+    return render(request, 'base/index.html', {})
