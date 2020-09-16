ec4de76b4c8785503d08ee2653e4045478e774f9
ravellys <lucas.ravellys@ufpe.br>
2020-09-16 14:34:18 -0300

    Entrega da Landing-Page

--- pypro/settings.py
+++ pypro/settings.py
@@ -165,7 +165,7 @@ if AWS_ACCESS_KEY_ID:
 
     # Static Assets
     # ------------------------------------------------------------------------------
-    STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATIC_S3_PATH = 'static'
     STATIC_ROOT = f'/{STATIC_S3_PATH}/'
     STATIC_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{STATIC_S3_PATH}/'
