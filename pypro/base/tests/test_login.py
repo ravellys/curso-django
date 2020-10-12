02a01dad6a81b9c60da598a7d02a52d499b257ec
ravellys <lucas.ravellys@ufpe.br>
2020-10-12 02:30:18 -0300

    Autenticação de Usuários

--- pypro/base/tests/test_login.py
+++ pypro/base/tests/test_login.py
@@ -68,5 +68,6 @@ def test_botao_sair_disponivel(resp_home_logado):
 def test_nome_usuario_logado_disponivel(resp_home_logado, usuario_logado):
     assert_contains(resp_home_logado, usuario_logado.first_name)
 
-def test_link_logout_disponivel(resp_home_logado) :
+
+def test_link_logout_disponivel(resp_home_logado):
     assert_contains(resp_home_logado, reverse('logout'))
